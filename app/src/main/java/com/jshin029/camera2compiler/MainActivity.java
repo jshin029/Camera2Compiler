3c81abd21f2f0261da9155b764e7e1cddd7bc472
John Shin <jshin029@ucr.edu>
2019-01-01 15:17:24 -0800

    write to txt file implemented

--- app/src/main/java/com/jshin029/camera2compiler/MainActivity.java
+++ app/src/main/java/com/jshin029/camera2compiler/MainActivity.java
@@ -1,14 +1,20 @@
 package com.jshin029.camera2compiler;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,15 +25,20 @@ import com.google.firebase.ml.vision.common.FirebaseVisionImage;
 import com.google.firebase.ml.vision.text.FirebaseVisionText;
 import com.google.firebase.ml.vision.text.FirebaseVisionTextRecognizer;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button cameraBtn;
+    Button cameraBtn, b_save;
     ImageView imageView;
     TextView textView;
     Bitmap bitmap;
+    EditText et_name, et_content;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,20 +47,63 @@ public class MainActivity extends AppCompatActivity {
         imageView = (ImageView) findViewById(R.id.imageView);
         textView = (TextView) findViewById(R.id.textView);
         cameraBtn = findViewById(R.id.cameraBtn);
+        et_name = (EditText) findViewById(R.id.et_name);
+        et_content = (EditText) findViewById(R.id.et_content);
+        b_save = (Button) findViewById(R.id.b_save);
+
+        b_save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String filename = et_name.getText().toString();
+                String content = et_content.getText().toString();
+
+                saveTextasFile(filename, content);
+            }
+        });
+
         cameraBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 dispatchTakePictureIntent();
             }
         });
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED){
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1000);
+        }
     }
 
-    static final int REQUEST_IMAGE_CAPTURE = 1;
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1000:
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this, "Permission granted!", Toast.LENGTH_SHORT).show();
+                }
+                else{
+                    Toast.makeText(this,"Permission not granted!", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+        }
+    }
 
-    private void dispatchTakePictureIntent() {
-        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            startActivityForResult(takePictureIntent, 2);
+    private void saveTextasFile(String filename, String content){
+        String fileName = filename + ".txt";
+
+        File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath(), fileName);
+
+        try{
+            FileOutputStream fos = new FileOutputStream(file);
+            fos.write(content.getBytes());
+            fos.close();
+            Toast.makeText(this, "Saved!", Toast.LENGTH_SHORT).show();
+        } catch (FileNotFoundException e){
+            e.printStackTrace();
+            Toast.makeText(this, "File not found!", Toast.LENGTH_SHORT).show();
+        } catch(IOException e){
+            e.printStackTrace();
+            Toast.makeText(this, "Error saving!", Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -92,10 +146,21 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_IMAGE_GALLERY = 2;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, 2);
+        }
+    }
+
+
     public void pick_image(View v)
     {
-        Intent i = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.INTERNAL_CONTENT_URI);
-        startActivityForResult(i,1);
+        Intent imagePickIntent = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.INTERNAL_CONTENT_URI);
+        startActivityForResult(imagePickIntent,1);
     }
 
     @Override
@@ -103,6 +168,7 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if(requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK)
         {
+            //assert data != null;
             Uri uri = data.getData();
             try
             {
@@ -113,11 +179,11 @@ public class MainActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
         }
-        else if (requestCode == 2 && resultCode == RESULT_OK) {
+        else if (requestCode == REQUEST_IMAGE_GALLERY && resultCode == RESULT_OK) {
+            //assert data != null;
             Bundle extras = data.getExtras();
             bitmap = (Bitmap) extras.get("data");
             imageView.setImageBitmap(bitmap);
         }
-
     }
 }
